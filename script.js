773b9b6053af68b2eb88a3fd473759641f551b65
rote1400 <rote1400@student.miun.se>
2024-02-03 12:44:58 +0100

    functionality

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 setInterval(setClock, 1000)
 
+const hourArrow = document.querySelector('[data-hour-arrow]');
+const minuteArrow = document.querySelector('[data-minute-arrow]');
+const secondArrow = document.querySelector('[data-second-arrow]');
 
 
 function setClock() {
     const currentDate = new Date();
-    const secondsRatio = currentDate.getHours() / 60;
-    const minutesRatio = (secondsRatio + currentDate.getHours()) / 60;
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
     const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+    setRotation(secondArrow, secondsRatio);
+    setRotation(minuteArrow, minutesRatio);
+    setRotation(hourArrow, hoursRatio);
 }
+
+function setRotation(element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
